3e24e6470adaf55977079fed842eec579de22254
Gourav Choubey <36809977+gouravch60@users.noreply.github.com>
2024-06-28 15:46:13 +0530

    Update navbar.js

--- frontend/src/components/layout/header/navbar.js
+++ frontend/src/components/layout/header/navbar.js
@@ -5,25 +5,37 @@ import Navbar from 'react-bootstrap/Navbar';
 import {NavLink} from 'react-router-dom' 
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserAlt, faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { useRef } from 'react';
 
 
 const NavbarComponent=()=>{
+    const collapseRef=useRef();
+    const navbarCollapseRef=useRef();
+    const handleCollapse=()=>{
+      if (navbarCollapseRef.current.classList.contains('show')) {
+        collapseRef.current.click();
+      }
+    }
     return(
-      <Navbar collapseOnSelect expand="lg" data-bs-theme="dark" className="bg-body-tertiary fixed-top">
+      <Navbar collapseOnSelect expand="md" data-bs-theme="dark" className="bg-body-tertiary fixed-top">
       <Container>
         <Navbar.Brand><img src='/logo.png' className='logo' /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" ref={collapseRef} />
+        <Navbar.Collapse id="responsive-navbar-nav" ref={navbarCollapseRef}>
         <Nav className="ms-auto">
-          <Nav.Link as={NavLink} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} exact to="/">Home</Nav.Link>
-          <Nav.Link as={NavLink} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/courses">Courses</Nav.Link>
-          <Nav.Link as={NavLink} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/events">Events</Nav.Link>
-          <Nav.Link as={NavLink} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/aboutus">About Us</Nav.Link>
-          <Nav.Link as={NavLink} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/gallery">Gallery</Nav.Link>
-          <Nav.Link as={NavLink} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/contactus">Contact Us</Nav.Link>
-          <NavDropdown title={<FontAwesomeIcon icon={faUserCircle} />} id="collapsible-nav-dropdown">
-            <NavDropdown.Item as={NavLink} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/login">Admin Login</NavDropdown.Item>
+          <Nav.Link as={NavLink} onClick={handleCollapse} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} exact to="/">Home</Nav.Link>
+          <Nav.Link as={NavLink} onClick={handleCollapse} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/courses">Courses</Nav.Link>
+          <Nav.Link as={NavLink} onClick={handleCollapse} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/events">Events</Nav.Link>
+          <Nav.Link as={NavLink} onClick={handleCollapse} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/aboutus">About Us</Nav.Link>
+          <Nav.Link as={NavLink} onClick={handleCollapse} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/gallery">Gallery</Nav.Link>
+          <Nav.Link as={NavLink} onClick={handleCollapse} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/contactus">Contact Us</Nav.Link>
+          <Nav.Link className="d-md-none" to="/login">
+            <FontAwesomeIcon icon={faUserAlt} />
+            <span className="ms-1">Admin Panel</span>
+          </Nav.Link>
+          <NavDropdown className="d-none d-md-block" title={<FontAwesomeIcon icon={faUserCircle} />} id="collapsible-nav-dropdown">
+            <NavDropdown.Item as={NavLink} className={({ isActive }) => isActive ? "navLink activeNavLink" : "navLink"} to="/login">Admin Panel</NavDropdown.Item>
           </NavDropdown>
         </Nav>
           {/* <Nav>
